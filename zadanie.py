dbd1db5486d076d0afb8010f2810bd5b4cbd09c1
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2025-03-26 21:27:21 +0300

    add exersizes 2 and 3

--- zadanie.py
+++ zadanie.py
@@ -17,4 +17,29 @@ with open('recipes.txt') as f:
 
             cook_book[key].append(sl)
 
-            
+print(cook_book)
+
+def get_shop_list_by_dishes(dishes,person_count):
+    result = {}
+    for j in dishes:
+        for m in cook_book[j]:
+            result[m[keys[0]]]={keys[2]:m[keys[1]]*person_count,keys[1]:m[keys[2]]}
+    return result
+
+print(get_shop_list_by_dishes(['Утка по-пекински'],2))
+
+fs,ln = ['1.txt','2.txt','3.txt'],[]
+for j in fs:
+    with open(j,'r',encoding='cp1251') as f:
+        d = 0
+        for j in f:
+            d+=1
+        ln.append(d)
+
+with open("result_t.txt",'w') as f:
+    for jj in sorted(ln):
+        f.write(f'{fs[ln.index(jj)]}\n')
+        f.write(f'{jj}\n')
+        with open(f'{fs[ln.index(jj)]}','r',encoding='cp1251') as fm:
+            for j in fm:
+                f.write(f'{j}')
