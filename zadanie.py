6b220f0584190f51b66383a52c242aa4b501552c
Danilchenko Mihail <mihaildanilchenko85@gmail.com>
2025-03-28 17:44:14 +0300

    modernizatin code for exesizes 1, 2

--- zadanie.py
+++ zadanie.py
@@ -1,6 +1,5 @@
-cook_book = {}
-
 with open('recipes.txt') as f:
+    cook_book = {}
     for l in (f):
         if l.count('|')==0 and len(l)>2:#выводим все названия
             cook_book[l[:-1]]=[]
@@ -17,29 +16,20 @@ with open('recipes.txt') as f:
 
             cook_book[key].append(sl)
 
-print(cook_book)
-
 def get_shop_list_by_dishes(dishes,person_count):
     result = {}
+    naimenovanie = ['ingredient','quantity','measure']
     for j in dishes:
-        for m in cook_book[j]:
-            result[m[keys[0]]]={keys[2]:m[keys[1]]*person_count,keys[1]:m[keys[2]]}
+        if cook_book.get(j) == None:
+            print(f"Блюда '{j}' нет в меню")
+        else:
+            for m in cook_book[j]:
+                if result.get(m[keys[0]])==None:
+                    result[m[naimenovanie[0]]] = {naimenovanie[2]:m[naimenovanie[1]]*person_count,
+                                                  naimenovanie[1]:m[naimenovanie[2]]}
+                else:
+                    result[m[naimenovanie[0]]] = {naimenovanie[2]:m[naimenovanie[1]]*person_count+result[m[naimenovanie[0]]][naimenovanie[-1]],
+                                                  naimenovanie[1]:m[naimenovanie[2]]}
     return result
-
-print(get_shop_list_by_dishes(['Утка по-пекински'],2))
-
-fs,ln = ['1.txt','2.txt','3.txt'],[]
-for j in fs:
-    with open(j,'r',encoding='cp1251') as f:
-        d = 0
-        for j in f:
-            d+=1
-        ln.append(d)
-
-with open("result_t.txt",'w') as f:
-    for jj in sorted(ln):
-        f.write(f'{fs[ln.index(jj)]}\n')
-        f.write(f'{jj}\n')
-        with open(f'{fs[ln.index(jj)]}','r',encoding='cp1251') as fm:
-            for j in fm:
-                f.write(f'{j}')
+# print(cook_book) 
+print(get_shop_list_by_dishes(['Омлет','Фахитос','Омлет'],5))
